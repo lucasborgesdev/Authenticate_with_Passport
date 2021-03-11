7cd1b7ad8685aba7ceeb137da8895d42d3cc200e
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2021-03-11 15:13:07 -0300

    feito metodo post e criptografando o pass

---
+++ backend/src/User.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+const user = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  password: String,
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  
+});
+
+
+export default mongoose.model("User", user);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import passportLocal from 'passport-local';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import bcrypt from 'bcryptjs';
-//import User from './User'
+import User from './User';
 import dotenv from 'dotenv';
 
 mongoose.connect("mongodb+srv://lucas:@compasso@cluster0.czj0x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
@@ -21,5 +21,35 @@ mongoose.connect("mongodb+srv://lucas:@compasso@cluster0.czj0x.mongodb.net/myFir
 
 
 //middleWare
+const app = express();
+app.use(express.json());
+app.use(cors({origin: "http://localhost:3000", credentials: true }))
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session())
+
+
+//Routes
+app.post('/register', async (req : Request, res : Response) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const newUser = new User({
+    username: req.body.username,
+    password: hashedPassword     
+  });
+  await newUser.save();
+  res.send("Success")
+
+});
+
+app.listen(4000, () => {
+  console.log("Server Started")
+} )
 
 
